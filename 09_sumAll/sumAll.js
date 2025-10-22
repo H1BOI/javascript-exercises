3009efa38a6058ed452b4769985204046cf79158
Chun Lin <132721625+H1BOI@users.noreply.github.com>
2025-10-22 00:10:43 -0400

    finished 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let sum = 0;
 
+    if (int1 < 0 || int2 < 0) {
+        return "ERROR";
+    }
+
+    if (Number.isInteger(int1) && Number.isInteger(int2)) {
+        ;
+    } else {
+        return "ERROR";
+    }
+
+    if (int1 < int2) {
+        var first = int1;
+        var second = int2;
+    } else {
+        var first = int2;
+        var second = int1;
+    }
+
+    for (i = first; i < second+1; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
