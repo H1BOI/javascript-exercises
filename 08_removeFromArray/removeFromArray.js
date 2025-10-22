550131e44bfd93a106068a6dc3e4fbd010aa44a5
Chun Lin <132721625+H1BOI@users.noreply.github.com>
2025-10-21 23:53:22 -0400

    finished 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...targets) {
+    const newArray = [];
+    
+    arr.forEach((item) => {
+        if (!targets.includes(item)) {
+        newArray.push(item);
+        }
+    });
+
+    return newArray;
 };
 
 // Do not edit below this line
